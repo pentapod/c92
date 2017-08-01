05dfd04f96763d51b6b34294af59e41667561680
spring-raining <fdonthavei@gmail.com>
2017-08-01 23:14:47 +0900

    Change assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  assetPrefix: isProd? '/c92' : '', // Set assetPrefix for GitHub pages
   exportPathMap: function() {
     return {
       '/': { page: '/' },
